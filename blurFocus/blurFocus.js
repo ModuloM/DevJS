f6bdc5af721cbd7019bd08035897f742d45b38c9
ModuloM <modulom@modulom.net>
2013-06-18 00:50:03 +0200

    It's steel not blury...

--- blurFocus/blurFocus.js
+++ blurFocus/blurFocus.js
@@ -1,13 +1,15 @@
 (function($) {
 
+	var fond0 = $('.fondEtat0'),
+		fond1 = $('.fondEtat1');
+
 	$('.focus').on("mouseover", function(event) {
-		console.log('on');
-		$('span.etat4').fadeOut(350);
-		$('span.etat3').fadeIn(350);
+		fond0.fadeOut(350);
+		fond1.fadeIn(350);
 	}).on("mouseleave", function() {
 		console.log('out');
-		$('span.etat4').fadeIn(350);
-		$('span.etat3').fadeOut(350);
+		fond1.fadeOut(350);
+		fond0.fadeIn(350);
 	});
 
 
